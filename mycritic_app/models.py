8ecccebb253a3b696c4eb9ce2c2917281460a6e9
Henry Johnson <henryjohnson@dhcp-wifi-8021x-155-41-12-140.bu.edu>
2017-04-24 19:23:24 -0400

    userprofile model

--- mycritic_app/models.py
+++ mycritic_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class SearchCache(models.Model):                   #####
     search_query = models.CharField(max_length=50) #####
     value = models.TextField()                     #####
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_name = mdels.CharField(max_length=150)
+    bio = models.TextField(max_length=500, blank=True)
+    email = models.CharField(max_length=50)
+    twitter = models.CharField(max_length=50)
